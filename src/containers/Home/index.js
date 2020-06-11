e275e753190aa73feec918fd33ba8fde4ef93f4c
Tassia Shibuya <tassia.shibuya@gmail.com>
2020-06-10 22:24:25 -0700

    add a couple of sentences to the about me and changed to size font

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -19,23 +19,27 @@ function HomePage() {
         <Grid.Column>
           <h1>Tassia Shibuya</h1>
           <p>
-            Hello! My name is Tassia Shibuya, I'm currently a student at the UC
+            Hello! My name is Tassia Shibuya, I just graduated from UC
             Berkeley coding bootcamp.
           </p>
           <p>
             I was born in Brazil- Sao Paulo, lived in London, Dublin and Lisbon,
-            my last job as an auditor brought me to California, which I feel in
+            my last job as an auditor brought me to California, which I fell in
             love with the place and that helped me with my decision to stay.
           </p>
           <p>
             Working with multiple teams and engineers at my previous job made
-            want to learn more about codinh. My hunger for knowledge and
+            want to learn more about coding. My hunger for knowledge and
             determination to turn information into action has contributed to my
             most recent sucess at UC Berkeley.
           </p>
           <p>
+            During the bootcamp as I learned and struggled to digest the fast paced learning curve from the boot-camp
+            I felt like wanted to dive even deeper into the computer program enviroment.
+          </p>
+          <p>
             When not online, I can be found in San Francisco, California with my
-            family, jogging, biking, trying out a new recipe in the kitchen,
+            family, jogging, biking, trying out a new recipe in the kitchen or 
             experimenting in gardening.
           </p>
           <br />
