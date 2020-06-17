c0d43eb5c8ec08f5dd5773d79582d940664aee77
Tassia Shibuya <tassia.shibuya@gmail.com>
2020-06-17 11:10:35 -0700

    wrote about me

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -19,8 +19,14 @@ function HomePage() {
         <Grid.Column>
           <h1>Tassia Shibuya</h1>
           <p>
-            Hello! My name is Tassia Shibuya, I just graduated from UC
-            Berkeley coding bootcamp.
+            Hello! My name is Tassia Shibuya,
+            I am a personable and passionate Web Developer with a background in Accounting and Finance 
+            with Full Stack Certificate from the University of California, Berkeley. 
+            I have honed my skills in Node.js, Axios, Express, SQL, JavaScript, CSS, HTML, React, Redux to develop 
+            meaningful server-side applications. I am able to collaborate and work with a team while solving problems 
+            in high-stress environments. An articulate individual who enjoys connecting with co-workers and is passionate 
+            about building applications that assist everyday people to make their lives easier. 
+            Looking to bring my attention to detail and technical skills to a company to grow and become a valuable asset.
           </p>
           <p>
             I was born in Brazil- Sao Paulo, lived in London, Dublin and Lisbon,
@@ -31,16 +37,16 @@ function HomePage() {
             Working with multiple teams and engineers at my previous job made
             want to learn more about coding. My hunger for knowledge and
             determination to turn information into action has contributed to my
-            most recent sucess at UC Berkeley.
-          </p>
-          <p>
-            During the bootcamp as I learned and struggled to digest the fast paced learning curve from the boot-camp
-            I felt like wanted to dive even deeper into the computer program enviroment.
+            most recent sucess in graduate the boot-camp, throughout as I learned and struggled to digest the fast paced 
+            learning curve from the boot-camp, I felt like wanted to dive even deeper into the computer program enviroment. 
           </p>
           <p>
             When not online, I can be found in San Francisco, California with my
             family, jogging, biking, trying out a new recipe in the kitchen or 
             experimenting in gardening.
+          </p>
+          <p>
+          Fun fact: I once participated in a Guitar Hero competition and I was the only woman in the semi finals.
           </p>
           <br />
           <a href="https://github.com/Tassim">
