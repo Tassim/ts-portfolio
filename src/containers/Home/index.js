dc4357a49d81739d6ab2558dbebddf4ce9462868
Tassia Shibuya <tassia.shibuya@gmail.com>
2020-06-18 15:00:20 -0700

    updated about me

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -8,45 +8,47 @@ import "./style.css";
 function HomePage() {
   return (
     <div>
-      {/* <Image src={Logo} size='large' /> */}
-      {/* <Button icon labelPosition="right" floated='right' as={Link} to="/work">
-          Projects
-          <Icon name="right arrow" />
-        </Button> */}
-
       <Grid columns={2} stackable>
-
         <Grid.Column>
-          <h1>Tassia Shibuya</h1>
+          <h1 className='nameTitle'>Tassia Shibuya</h1>
           <p>
-            Hello! My name is Tassia Shibuya,
-            I am a personable and passionate Web Developer with a background in Accounting and Finance 
-            with Full Stack Certificate from the University of California, Berkeley. 
-            I have honed my skills in Node.js, Axios, Express, SQL, JavaScript, CSS, HTML, React, Redux to develop 
-            meaningful server-side applications. I am able to collaborate and work with a team while solving problems 
-            in high-stress environments. An articulate individual who enjoys connecting with co-workers and is passionate 
-            about building applications that assist everyday people to make their lives easier. 
-            Looking to bring my attention to detail and technical skills to a company to grow and become a valuable asset.
+          Hello! My name is Tassia Shibuya, I was born in Brazil - Sao Paulo, 
+          lived in London, Dublin and Lisbon, my last job as an auditor brought me 
+          to California, which I immediately fell in love with and decided to set 
+          my roots down.
           </p>
           <p>
-            I was born in Brazil- Sao Paulo, lived in London, Dublin and Lisbon,
-            my last job as an auditor brought me to California, which I fell in
-            love with the place and that helped me with my decision to stay.
+          I am a personable and passionate Web Developer with a background in 
+          Accounting and Finance and a Full Stack Certificate from the University 
+          of California, Berkeley. While earning the Full Stack Certificate I 
+          developed skills in Node.js, Axios, Express, SQL, JavaScript, CSS, HTML, 
+          React, Redux in order to create meaningful server-side applications. My 
+          background in Accounting and Finance has taught me to effectively 
+          collaborate in a team setting while solving problems in high-stress 
+          environments. I’m an articulate individual who enjoys connecting with 
+          co-workers and is passionate about building applications that assist 
+          everyday people with everyday life. I look forward to bringing my newly 
+          acquired technical coding skills to a company to assist with its growth 
+          and success.
           </p>
           <p>
-            Working with multiple teams and engineers at my previous job made
-            want to learn more about coding. My hunger for knowledge and
-            determination to turn information into action has contributed to my
-            most recent sucess in graduate the boot-camp, throughout as I learned and struggled to digest the fast paced 
-            learning curve from the boot-camp, I felt like wanted to dive even deeper into the computer program enviroment. 
+          Working with multiple teams and engineers at my previous job made want 
+          to learn more about coding. My hunger for knowledge and determination 
+          to turn information into action has contributed to my most recent 
+          success in graduating the boot-camp. Throughout the course my desire 
+          to dive deeper into the computer program environment continued to build 
+          and my confidence in the fact that I found my new passion continued to 
+          grow. 
           </p>
           <p>
-            When not online, I can be found in San Francisco, California with my
-            family, jogging, biking, trying out a new recipe in the kitchen or 
-            experimenting in gardening.
+          When not online, I can be found in San Francisco, California enjoying 
+          outdoor activities with my handsome husband and kids such as jogging, 
+          biking, or experimenting with gardening. I’m also an aspiring chef and 
+          can often be found in the kitchen cooking up a new recipe.
           </p>
           <p>
-          Fun fact: I once participated in a Guitar Hero competition and I was the only woman in the semi finals.
+          Fun fact: I once participated in a Guitar Hero competition and I was 
+          the only woman in the semifinals.
           </p>
           <br />
           <a href="https://github.com/Tassim">
